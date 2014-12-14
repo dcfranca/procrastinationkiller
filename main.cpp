41220b8f717e0a08975065f965710d5bc267365c
Daniel Franca <daniel.franca@gmail.com>
2014-12-14 22:51:41 +0100

    commented systray

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QSystemTrayIcon>
+#include <QMessageBox>
+#include <QAction>
+#include <QMenu>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +12,40 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    /*if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(0, QObject::tr("Systray"),
+                                 QObject::tr("I couldn't detect any system tray "
+                                             "on this system."));
+        return 1;
+    }
+
+    QObject *root = 0;
+    if (engine.rootObjects().size() > 0)
+    {
+        root = engine.rootObjects().at(0);
+
+        QAction *minimizeAction = new QAction(QObject::tr("Mi&nimize"), root);
+        root->connect(minimizeAction, SIGNAL(triggered()), root, SLOT(hide()));
+        QAction *maximizeAction = new QAction(QObject::tr("Ma&ximize"), root);
+        root->connect(maximizeAction, SIGNAL(triggered()), root, SLOT(showMaximized()));
+        QAction *restoreAction = new QAction(QObject::tr("&Restore"), root);
+        root->connect(restoreAction, SIGNAL(triggered()), root, SLOT(showNormal()));
+        QAction *quitAction = new QAction(QObject::tr("&Quit"), root);
+        root->connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+        QMenu *trayIconMenu = new QMenu();
+        trayIconMenu->addAction(minimizeAction);
+        trayIconMenu->addAction(maximizeAction);
+        trayIconMenu->addAction(restoreAction);
+        trayIconMenu->addSeparator();
+        trayIconMenu->addAction(quitAction);
+
+        QSystemTrayIcon *trayIcon = new QSystemTrayIcon(root);
+        trayIcon->setContextMenu(trayIconMenu);
+        trayIcon->setIcon(QIcon(":/img/icon.png"));
+        trayIcon->show();
+    }*/
 
     return app.exec();
 }
