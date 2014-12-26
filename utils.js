2cd6f7780faf70f55a0f948663884c3f6e969bbf
Daniel Franca <daniel.franca@gmail.com>
2014-12-26 12:58:31 +0100

    set the current Index to the newly created task

--- utils.js
+++ utils.js
@@ -1,6 +1,11 @@
-function addTask(taskModel, task) {
+/*
+  General functions
+*/
+
+function addTask(taskModel, task, tasksList) {
     if (task.length > 0) {
         tasksModel.insert(0, {task: task, time: "30m", remaining: "00:30:00", state: "paused", done: false})
+        tasksList.currentIndex = 0;
     }
 }
 
