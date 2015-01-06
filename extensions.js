d76ab6450bb2a820c4d2a30f910380f453e6dd8a
Daniel Franca <daniel.franca@gmail.com>
2015-01-07 00:01:42 +0100

    loading extensions

--- extensions.js
+++ extensions.js
@@ -1,4 +1,19 @@
-function func() {
+/*
+  Functions to handle extensions
+*/
 
+function createExtensionComponent(element, parent) {
+    var extensionsList = extMng.extensions;
+    var baseDir = "qrc:/extensions/";
+    for (var x=0; x < extensionsList.length; x++) {
+        var component = Qt.createComponent(baseDir + extensionsList[x] + "/" + element);
+        if (component.status === Component.Ready) {
+            console.log("-- Component " + element + " loaded");
+            component.createObject(parent);
+        }
+        else if (component.status === Component.Error) {
+            console.log(component.errorString());
+        }
+    }
 }
 
